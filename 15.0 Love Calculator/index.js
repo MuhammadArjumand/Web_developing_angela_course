eb6ce171c9c2f32a4dde86d986983d143f3b4d0f
MuhammadArjumand <muhammadarjumandkhan01@gmail.com>
2025-05-27 16:12:48 +0500

    Love Calculator

--- 15.0 Love Calculator/index.js
+++ 15.0 Love Calculator/index.js
@@ -3,4 +3,8 @@ f=prompt("His/Her name");
 var a = Math.random();
 a = a*100;
 a=Math.floor(a)+1;
-alert(e+ " and "+ f +" have a love score of "+ a);
+if (a > 70){
+    alert("Perfect match! Your love score is " +a+"%");
+} else {
+    alert("Your love score is "+a+"%");
+}

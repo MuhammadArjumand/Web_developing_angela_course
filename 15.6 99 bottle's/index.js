3f26699e116e81910e534046ea237e1f0d8ac7f9
MuhammadArjumand <muhammadarjumandkhan01@gmail.com>
2025-05-29 14:08:15 +0500

    99 Bottles

---
+++ 15.6 99 bottle's/index.js
@@ -0,0 +1,7 @@
+var count = 99;
+
+while (count > 0) {
+    alert(count + " bottles of beer on the wall, " +count + " bottles of beer. Take one down and pass it around, " +(count - 1) + " bottles of beer on the wall.");count--;
+}
+
+alert("No more bottles of beer on the wall, no more bottles of beer.");

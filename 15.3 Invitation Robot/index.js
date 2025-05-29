f0af6922db455b877563009a87258be573add61c
MuhammadArjumand <muhammadarjumandkhan01@gmail.com>
2025-05-29 10:49:17 +0500

    Invitation robot

---
+++ 15.3 Invitation Robot/index.js
@@ -0,0 +1,7 @@
+var invite = ["abdullah","shahzaib","hassan","usama","daniyal"];
+var name=prompt("what is your name");
+if (invite.includes(name)){
+    alert("You are invited");
+} else {
+    alert("Not invited");
+}
